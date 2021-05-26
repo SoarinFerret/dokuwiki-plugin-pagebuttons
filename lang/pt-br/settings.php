d59293dc53108d826c5f8882e90f9a191c129e6d
Eduardo Mozart de Oliveira <2974895+eduardomozart@users.noreply.github.com>
2021-05-26 12:27:09 -0300

    Update settings.php

--- lang/pt-br/settings.php
+++ lang/pt-br/settings.php
@@ -7,5 +7,5 @@
 $lang['hideDelete']     = 'Desabilitar o botão de Excluir página';
 $lang['hideNewPage']    = 'Desabilitar o botão de Nova página';
 $lang['hideNewFolder']  = 'Desabilitar o botão de Nova pasta';
-$lang['usePrompt']      = 'Usar caixa de diálogo JS ao invés de modal - alguns temas não implementam corretamente modals';
+$lang['usePrompt']      = 'Usar caixa de diálogo JS ao invés de modal - alguns templates não implementam corretamente modals';
 $lang['onlyShowNewButtonsOnStart'] = 'Somente exibir os botões Nova página e Nova pasta em páginas :start';
