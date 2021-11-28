935884599a3e75aecdac07f11f1bfd88ec3c0b29
jimmy_janisson <jimmy.janisson@gmail.com>
2021-11-28 01:45:24 +0100

    add fr translation

---
+++ lang/fr/lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Page Buttons plugin - English language file
+ * 
+ * @copyright (c) 2020 Cody Ernesti
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Cody Ernesti
+ *
+ *  Modified from: https://github.com/dregad/dokuwiki-plugin-deletepagebutton
+ *
+ *   Original license info:
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['newpage_menu_item']      = 'Nouvelle page';
+$lang['delete_menu_item']       = 'Supprimer la page';
+$lang['newfolder_menu_item']    = 'Nouveau dossier';
+$lang['deleted_ok']             = 'Page supprimée';
+
+$lang['js']['delete_title']     = $lang['delete_menu_item'];
+$lang['js']['newpage_title']    = $lang['newpage_menu_item'];
+$lang['js']['newfolder_title']  = $lang['newfolder_menu_item'];
+$lang['js']['delete_confirm']   = 'Êtes-vous sûr de vouloir supprimer cette page ?';
+$lang['js']['newpage_prompt']   = 'Quelle est le nom de page que vous souhaitez créer ?';
+$lang['js']['newfolder_prompt'] = 'Quel est le nom du dossier que vous souhaitez créer ?';
+$lang['js']['btn_ok']           = 'OK';
+$lang['js']['btn_cancel']       = 'Annuler';
---
+++ lang/fr/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * English language file for config
+ *
+ */
+
+$lang['hideDelete']     = 'Désactive le bouton "Supprimer la page"';
+$lang['hideNewPage']    = 'Désactive le bouton "Nouvelle page"';
+$lang['hideNewFolder']  = 'Désactive le bouton "Nouveau dossier"';
+$lang['usePrompt']      = 'Utiliser un prompt JS au lieu des modales - certains thèmes implémentent mal les modales';
+$lang['onlyShowNewButtonsOnStart'] = 'Ne montrer les boutons "Nouvelle page" et "Nouveau dossier" que sur les pages de démarrage (:start par défaut)';
