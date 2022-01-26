06e260be6cb795a39b3442f724ab20b0f7fa232d
Viktor Kristian <41580451+vkristian@users.noreply.github.com>
2022-01-26 07:00:13 +0100

    added translation to sk language (#9)

---
+++ lang/sk/lang.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Page Buttons plugin - slovak language file
+ * 
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Viktor Kristian <vkristian@gmail.com>
+ *
+ *  Modified from: https://github.com/dregad/dokuwiki-plugin-deletepagebutton
+ *
+ *   Original license info:
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['newpage_menu_item']      = 'Nová stránka';
+$lang['delete_menu_item']       = 'Zmazať stránku';
+$lang['newfolder_menu_item']    = 'Nový pričinok';
+$lang['deleted_ok']             = 'Stránka úspešne zmazaná';
+
+$lang['js']['delete_title']     = $lang['delete_menu_item'];
+$lang['js']['newpage_title']    = $lang['newpage_menu_item'];
+$lang['js']['newfolder_title']  = $lang['newfolder_menu_item'];
+$lang['js']['delete_confirm']   = 'Ste si istý, že chcete zmazať túto stránku?';
+$lang['js']['newpage_prompt']   = 'Ako sa má volať stránka, ktorú si želáte vytvoriť?';
+$lang['js']['newfolder_prompt'] = 'Ako sa má volať priečinok, ktorý si želáte vytvoriť?';
+$lang['js']['btn_ok']           = 'OK';
+$lang['js']['btn_cancel']       = 'Zrušiť';
---
+++ lang/sk/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Slovak language file for config
+ *
+ */
+
+$lang['hideDelete']     = 'Zakázať tlačidlo Zmazať';
+$lang['hideNewPage']    = 'Zakázať tlačidlo Nová stránka';
+$lang['hideNewFolder']  = 'Zakázať tlačidlo Nový priečinok';
+$lang['usePrompt']      = 'Použiť JavaScriptový prompt namiesto modálneho - niektoré témy neimplementujú modálne prompty správne.';
+$lang['onlyShowNewButtonsOnStart'] = 'Zobrazovať tlačidlá Nová stránka a Nový priečinok len na stránkach :start';
