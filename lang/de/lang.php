b8e3cec0da303167d48394b5238b8011207f69c1
mschlenstedt <Michael@schlenn.net>
2023-05-30 06:17:09 +0200

    German translation (#15)

---
+++ lang/de/lang.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Page Buttons plugin - German language file
+ *
+ * @copyright (c) 2022 Michael Schlenstedt
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Cody Ernesti
+ *
+ *  Modified from: https://github.com/dregad/dokuwiki-plugin-deletepagebutton
+ *
+ *   Original license info:
+ *
+ * @copyright (c) 2020 Damien Regad
+ * @license GPLv2 or later (https://www.gnu.org/licenses/old-licenses/gpl-2.0.html)
+ * @author  Damien Regad
+ */
+
+$lang['newpage_menu_item']      = 'Neue Seite';
+$lang['delete_menu_item']       = 'Seite löschen';
+$lang['newfolder_menu_item']    = 'Neuer Unterordner';
+$lang['deleted_ok']             = 'Seite erfolgreich gelöscht';
+
+$lang['js']['delete_title']     = $lang['delete_menu_item'];
+$lang['js']['newpage_title']    = $lang['newpage_menu_item'];
+$lang['js']['newfolder_title']  = $lang['newfolder_menu_item'];
+$lang['js']['delete_confirm']   = 'Bist Du sicher, dass Du die Seite löschen möchtest?';
+$lang['js']['newpage_prompt']   = 'Welche Unterseite möchtest Du anlegen?';
+$lang['js']['newfolder_prompt'] = 'Welchen Unterordner möchtest Du anlegen?';
+$lang['js']['btn_ok']           = 'OK';
+$lang['js']['btn_cancel']       = 'Abbrechen';
---
+++ lang/de/settings.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * English language file for config
+ *
+ */
+
+$lang['hideDelete']     = 'Deaktiviere den Löschen Button';
+$lang['hideNewPage']    = 'Deatkviere den Neue Seite Button';
+$lang['hideNewFolder']  = 'Deaktiviere den Neuen Unterordner Button';
+$lang['usePrompt']      = 'Nutze JS Abfrage anstelle der Modalmethode - einige Themes haben Modals nicht implementiert';
+$lang['onlyShowNewButtonsOnStart'] = 'Zeige die Neue Seite und Neuer Unterordner Button nur auf :start Seiten';
